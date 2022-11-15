3a6d1096cdc676b79db8b8b9fd9416aaac3c4dcb
DenisAstashkin <ast.den27@gmail.com>
2022-11-15 15:35:58 +0400

    Del NodeModel

--- Parser/Model/node_model.py
+++
@@ -1,5 +0,0 @@
-class Node(object):
-    def __init__(self, start_city, finish_city, weight):
-        self.start_city = start_city
-        self.finish_city = finish_city
-        self.weight = weight
